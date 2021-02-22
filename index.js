d0f4ee7287675ec6b5ef6017c814f65c29413cc2
Esraa Sami <74627798+esraasami20@users.noreply.github.com>
2021-02-23 00:07:15 +0200

    new

--- index.js
+++ index.js
@@ -28,24 +28,24 @@ app.use('/Blog', blogrout);
 app.use('/User', userrout);
 
 app.use('*', (req, res, next) => {
-    res.status(404).json({ err: 'NOT_FOUND' });
+  res.status(404).json({ err: 'NOT_FOUND' });
 });
 app.use((err, req, res, next) => {
 
-    console.error(err);
-    if (err instanceof mongoose.Error.ValidationError) {
-        return res.status(422).json(err.errors);
-    }
-    if (err.code === 11000) {
-        res.status(422).json({ statusCode: 'ValidationError', property: err.keyValue });
-    }
-    if (err.message === 'UN_AUTHENTICATED') {
-        res.status(401).json({ statusCode: 'UN_AUTHENTICATED' });
-    }
-    res.status(503).end();
+  console.error(err);
+  if (err instanceof mongoose.Error.ValidationError) {
+    return res.status(422).json(err.errors);
+  }
+  if (err.code === 11000) {
+    res.status(422).json({ statusCode: 'ValidationError', property: err.keyValue });
+  }
+  if (err.message === 'UN_AUTHENTICATED') {
+    res.status(401).json({ statusCode: 'UN_AUTHENTICATED' });
+  }
+  res.status(503).end();
 });
 const { PORT = 3000 } = process.env;
 
 app.listen(PORT, () => {
-    console.log('APP is up and ready on:', PORT);
+  console.log('APP is up and ready on:', PORT);
 })
